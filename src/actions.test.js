00516ffef6ebe89116d524bcc17ff3b6b4fbbad9
Sarah Shuey <sarah.shuey@me.com>
2017-10-02 13:03:26 -0500

    2 action test working

--- src/actions.test.js
+++ src/actions.test.js
@@ -10,4 +10,7 @@ describe("Test for action types", function() {
   test('Returns action with my todo', function() {
     expect(createTodo('Test')).toEqual({type:CREATE_TODO, payload: 'Test'})
   })
+  test('Returns correct ID', function() {
+  expect(toggleTodo(1)).toEqual({type:TOGGLE_TODO, payload: 1});
+})
 })
