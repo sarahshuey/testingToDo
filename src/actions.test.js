31a8221bfe24cff79bf7511094a70b5cb4ac24ec
Sarah Shuey <sarah.shuey@me.com>
2017-10-02 13:02:39 -0500

    1 action test working

---
+++ src/actions.test.js
@@ -0,0 +1,13 @@
+import {createTodo} from './actions'
+import {CREATE_TODO} from './actions'
+import {toggleTodo} from './actions'
+import {TOGGLE_TODO} from './actions'
+import {filterTodos} from './actions'
+import {FILTER_TODOS} from './actions';
+
+
+describe("Test for action types", function() {
+  test('Returns action with my todo', function() {
+    expect(createTodo('Test')).toEqual({type:CREATE_TODO, payload: 'Test'})
+  })
+})
