6b7f3bff43e1e84d00300040e3678f0160dde5d7
Sarah Shuey <sarah.shuey@me.com>
2017-10-02 12:59:27 -0500

    reducer test working and snapshot test working

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -10,15 +10,17 @@ describe("CREATE_TODO", function () {
         expect(state.todos).toHaveLength(1);
         expect(state.todos[0]).toEqual({done:false,id: 1,text:'Test'});
     })
-})
-
-describe("FILTER_TODOS", function () {
-    test("filters todos", function () {
-        const initialState = {nextId:1,todos:[],filter:'all'}
-        const state = reducer(initialState, filterTodos("Test"));
-        expect(state.todos).toHaveLength([]);
-        expect(state.todos[[]]).toEqual({filter: {
-            $set: action.payload
-        }});
+    test("TOGGLE_TODO", function() {
+     const initialState = {todos: [], nextId: 1, filter: 'all'};
+     let state = reducer(initialState, createTodo("Test"));
+     state = reducer(state, toggleTodo(1));
+     expect(state.todos[0].done).toEqual(true)
+   })
+   test("FILTER_TODOS", function() {
+      const initialState = {todos: [], nextId: 1, filter: 'all'};
+      let state = reducer(initialState, createTodo("Test"));
+      state = reducer(state, toggleTodo(1));
+      state = reducer(state, filterTodos('completed'));
+      expect(state.todos).toHaveLength(1);
     })
 })
